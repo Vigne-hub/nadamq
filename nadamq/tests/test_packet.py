3de41db16ab4d2428dd70e99e255fd3e53aa777e
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2024-09-29 17:45:58 -0400

    convert nosetest to pytests

--- nadamq/tests/test_packet.py
+++ nadamq/tests/test_packet.py
@@ -1,16 +1,14 @@
 # coding: utf-8
-from nose.tools import eq_, ok_
+import pytest
 from nadamq.NadaMq import cPacket, PACKET_TYPES
-
 
 def test_buffer_auto():
     """
     Test auto-buffer based on data content.
     """
     p = cPacket(iuid=1234, type_=PACKET_TYPES.DATA, data=b'hello, world!')
-    eq_(p.buffer_size, len(b'hello, world!'))
-    eq_(p.data(), b'hello, world!')
-
+    assert p.buffer_size == len(b'hello, world!')
+    assert p.data() == b'hello, world!'
 
 def test_buffer_larger_than_data():
     """
@@ -18,43 +16,37 @@ def test_buffer_larger_than_data():
     """
     p = cPacket(iuid=1234, type_=PACKET_TYPES.DATA, data=b'hello, world!',
                 buffer_size=1024)
-    eq_(p.data(), b'hello, world!')
-    eq_(p.buffer_size, 1024)
-
+    assert p.data() == b'hello, world!'
+    assert p.buffer_size == 1024
 
 def test_buffer():
     """
     Test setting allocating buffer without setting data contents.
     """
     p = cPacket(iuid=1234, type_=PACKET_TYPES.DATA, buffer_size=1024)
-    eq_(p.buffer_size, 1024)
-    eq_(p.data(), b'')
-
+    assert p.buffer_size == 1024
+    assert p.data() == b''
 
 def test_no_buffer():
     """
     Test no buffer allocation.
     """
     p = cPacket(iuid=1234, type_=PACKET_TYPES.DATA)
-    try:
+    with pytest.raises(RuntimeError, match="No buffer has been set/allocated."):
         p.data()
-    except RuntimeError as e:
-        ok_('No buffer has been set/allocated.' in str(e))
-
 
 def test_default_packet():
     """
     Test default packet allocation, i.e., no iuid, type, or data/buffer.
     """
     p = cPacket()
-    eq_(p.iuid, 0)
-    eq_(p.type_, PACKET_TYPES.NONE)
-    eq_(p.buffer_size, 0)
-
+    assert p.iuid == 0
+    assert p.type_ == PACKET_TYPES.NONE
+    assert p.buffer_size == 0
 
 def test_cPacket():
     """
     Test serialization of ``cPacket`` containing ``"hello, world!"``.
     """
     packet = cPacket(data=b'hello, world!', type_=PACKET_TYPES.DATA)
-    eq_(packet.tobytes(), b'|||\x00\x00d\x00\rhello, world!\xfa5')
+    assert packet.tobytes() == b'|||\x00\x00d\x00\rhello, world!\xfa5'
--- nadamq/tests/test_packet_parser.py
+++ nadamq/tests/test_packet_parser.py
@@ -1,33 +1,29 @@
 # coding: utf-8
 from nadamq.NadaMq import cPacket, parse_from_string, PACKET_TYPES
-import nose.tools as nt
-
+import pytest
 
 def test_parse_data():
     a = cPacket(iuid=1234, type_=PACKET_TYPES.DATA, data=b'hello')
     b = parse_from_string(a.tobytes())
 
-    nt.eq_(a.type_, b.type_)
-    nt.eq_(a.iuid, b.iuid)
-    nt.eq_(a.crc, b.crc)
-    nt.eq_(a.data(), b.data())
-
+    assert a.type_ == b.type_
+    assert a.iuid == b.iuid
+    assert a.crc == b.crc
+    assert a.data() == b.data()
 
 def test_parse_ack():
     a = cPacket(iuid=1010, type_=PACKET_TYPES.ACK)
     b = parse_from_string(a.tobytes())
 
-    nt.eq_(a.type_, b.type_)
-    nt.eq_(a.iuid, b.iuid)
-
+    assert a.type_ == b.type_
+    assert a.iuid == b.iuid
 
 def test_parse_nack():
     a = cPacket(iuid=4321, type_=PACKET_TYPES.NACK)
     b = parse_from_string(a.tobytes())
 
-    # TODO Should parse fail with `NACK` type?  It currently does.
-    nt.assert_false(b)
-
+    # TODO Should parse fail with `NACK` type? It currently does.
+    assert not b
 
 def test_parse_id_response():
     """
@@ -37,11 +33,11 @@ def test_parse_id_response():
                 data=b'{"id": "my device name"}')
     b = parse_from_string(a.tobytes())
 
-    nt.eq_(a.type_, b.type_)
-    nt.eq_(a.iuid, b.iuid)
-    nt.eq_(a.crc, b.crc)
-    # nt.eq_(a.data(), b.data())
-
+    assert a.type_ == b.type_
+    assert a.iuid == b.iuid
+    assert a.crc == b.crc
+    # Uncomment if the data equality is required:
+    # assert a.data() == b.data()
 
 def test_parse_id_request():
     """
@@ -50,5 +46,5 @@ def test_parse_id_request():
     a = cPacket(iuid=1234, type_=PACKET_TYPES.ID_REQUEST)
     b = parse_from_string(a.tobytes())
 
-    nt.eq_(a.type_, b.type_)
-    nt.eq_(a.iuid, b.iuid)
+    assert a.type_ == b.type_
+    assert a.iuid == b.iuid
